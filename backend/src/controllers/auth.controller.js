412423d8132f23464392c45f04a4e01cac4fbf7d
Madhur Bhawsar <madhurbhawsar7@gmail.com>
2025-11-02 00:36:06 +0530

    login added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
+import  jwt  from "jsonwebtoken";
 import userModel from "../models/user.model.js"
-import { hashPassword } from "../utils/hash.js";
+import { hashPassword, comparePassword } from "../utils/hash.js";
 
 
 
@@ -29,5 +30,35 @@ export const register = async (req, res) => {
     } catch (error) {
         console.error("Service Error:", error);
         return { status: 500, msg: "Failed to register user" };
+    }
+}
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    // console.log(email)
+    try {
+        const user = await userModel.findOne({
+            email
+        })
+
+        console.log(user.password)
+
+        const check = await comparePassword(password, user.password);
+        console.log(check);
+        if (!check) {
+            return res.json({
+                msg: "invalid credentials"
+            })
+        }
+        const token = await jwt.sign({id:user._id} , 'madhur')
+
+        return res.json({
+            msg:"login succesful",
+            token:token,
+            user
+        })
+    } catch (error) {
+        console.log(error)
     }
 }
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
 const router = express.Router();
-import { register } from '../controllers/auth.controller.js';
+import { register ,login } from '../controllers/auth.controller.js';
 
 
 router.post('/register' , register)
+router.post('/login' , login)
+
 
 export default router;
