a1e9bbef2db34b3977e420cd24ec46205ae64541
Madhur Bhawsar <madhurbhawsar7@gmail.com>
2025-11-02 00:21:57 +0530

    Register added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,29 +1,33 @@
 import userModel from "../models/user.model.js"
+import { hashPassword } from "../utils/hash.js";
+
 
 
 export const register = async (req, res) => {
-    const { email, password } = req.body
-    const check = await userModel.find({
-        email: email
-    })
-
-    if (check) {
-        console.log('user a;ready exists')
-        return res.json({
-            msg: 'user already existassassss'
-        })
-    }
+    const { name, email, password } = req.body
     try {
+        const existingUser = await userModel.findOne({ email });
+
+        if (existingUser) {
+            console.log("User already exists");
+            return res.status(400).json({
+                msg: "User already exists",
+            });
+        }
+
+        const hashedPassword = await hashPassword(password);
         const user = await userModel.create({
-            name: 'madhur',
-            email: email,
-            password: password
+            name,
+            email,
+            password: hashedPassword
         })
-        res.json({
-            msg: 'working',
-            user
+        return res.json({
+            status: 201,
+            msg: "User registered successfully",
+            data: { id: user._id, name: user.name, email: user.email },
         })
     } catch (error) {
-        console.log("errorrr")
+        console.error("Service Error:", error);
+        return { status: 500, msg: "Failed to register user" };
     }
 }
